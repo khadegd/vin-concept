8364304d40634ee76d9c8b0839eb0b2d9cc03ba9
Ganesh Khade <ganesh.khade@outlook.com>
2020-12-01 13:04:15 +0530

    Basic decoder component in place

--- ui/App.vue
+++ ui/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <VINDecoder/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VINDecoder from './components/VINDecoder.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    VINDecoder
   }
 }
 </script>
---
+++ ui/components/VINDecoder.vue
@@ -0,0 +1,37 @@
+<template>
+  <main class="form-signin">
+    <form>
+      <h1 class="h1 mb-3 fw-normal">VIN decoder</h1>
+      <label for="inputVIN" class="visually-hidden">VIN</label>
+      <input v-model="inputVIN"
+             @change="validateVIN"
+             type="text"
+             id="inputVIN"
+             class="form-control"
+             placeholder="VIN"
+             required autofocus>
+      <br>
+      <p>{{ inputVIN }}</p>
+    </form>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'VINDecoder',
+  props: {
+  },
+  data() {
+    return {
+      inputVIN: null,
+      isValid: false,
+      count: 0
+    }
+  },
+  methods: {
+    validateVIN() {
+    // logic to validate vin
+    }
+  }
+}
+</script>
