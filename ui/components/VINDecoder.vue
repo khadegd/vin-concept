8c2015581460f94802fae2111c495d3834068a1e
Ganesh Khade <ganesh.khade@outlook.com>
2020-12-01 14:41:35 +0530

    VIN validation with check digit implemented

--- ui/components/VINDecoder.vue
+++ ui/components/VINDecoder.vue
@@ -1,17 +1,19 @@
 <template>
   <main class="form-signin">
-    <form>
+    <form @submit.prevent="validateVIN">
       <h1 class="h1 mb-3 fw-normal">VIN decoder</h1>
       <label for="inputVIN" class="visually-hidden">VIN</label>
       <input v-model="inputVIN"
-             @change="validateVIN"
+             @keyup="validateVIN"
              type="text"
              id="inputVIN"
              class="form-control"
              placeholder="VIN"
              required autofocus>
       <br>
-      <p>{{ inputVIN }}</p>
+      <p v-if="isValid">{{ inputVIN }} is valid</p>
+      <p v-else>{{ inputVIN }} is invalid</p>
+      <button class='btn btn-primary'>Submit</button>
     </form>
   </main>
 </template>
@@ -30,7 +32,37 @@ export default {
   },
   methods: {
     validateVIN() {
-    // logic to validate vin
+      if(this.inputVIN.length !== 17 ) {
+        this.isValid = false;
+        return
+      }
+      let calculatedCheckDigit = this.calculateCheckDigit(this.inputVIN);
+      if (calculatedCheckDigit === parseInt(this.inputVIN[8])) {
+        this.isValid = true;
+      } else {
+        this.isValid = false;
+      }
+    },
+    decodeValueOf(char) {
+      //Transliteration key: values for VIN decoding
+      return '0123456789.ABCDEFGH..JKLMN.P.R..STUVWXYZ'.indexOf(char) % 10;
+    },
+    getWeightOf(index) {
+      // weights assigned for each 17 digits
+      let weights = '8765432X098765432';
+      // every weight stands for it's own value
+      // except x represents 10
+      let values = '0123456789X';
+      return values.indexOf(weights[index]);
+    },
+    calculateCheckDigit(vin) {
+      // Ref - https://en.wikipedia.org/wiki/Vehicle_identification_number#Check-digit_calculation
+      let total = 0;
+      for (let i = 0; i < 17; ++i) {
+        total += this.decodeValueOf(vin[i]) * this.getWeightOf(i);
+      }
+      let result = total % 11;
+      return result === 10 ? X : result;
     }
   }
 }
