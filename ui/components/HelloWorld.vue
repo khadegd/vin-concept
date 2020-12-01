1fb51fda355f0b893f9a1a06e257fe37d2e5a581
Ganesh Khade <ganesh.khade@outlook.com>
2020-12-01 09:24:49 +0530

    Restructure, Bootstrap 5, requirements freeze

--- ui/components/HelloWorld.vue
+++ ui/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>{{ msg }}</h1>
-  <button @click="count++">count is: {{ count }}</button>
+  <button class='btn btn-primary' @click="count++">count is: {{ count }}</button>
   <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
 </template>
 
